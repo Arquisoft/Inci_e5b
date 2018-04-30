51ea8e47f2ee2a489f6aeffd436c79c3c2026547
CarlosSanabriaM <uo250707@uniovi.es>
2018-04-30 21:54:52 +0200

    Quitado comentario.

--- InciDashboard_e5b/src/main/java/uo/asw/inciDashboard/currentIncidences/ReceiveFilteredIncidenceImpl.java
+++ InciDashboard_e5b/src/main/java/uo/asw/inciDashboard/currentIncidences/ReceiveFilteredIncidenceImpl.java
@@ -14,13 +14,7 @@ import uo.asw.dbManagement.model.Operator;
 public class ReceiveFilteredIncidenceImpl implements ReceiveFilteredIncidence {
 	
 	List<Incidence> listaincidencias = new ArrayList<Incidence>();
-	
-	/* TODO
-	 * El método ReceiveFilteredIncidence se encarga de recibir un objeto Incidence del componente Filter.
-	 * La información de dicho objeto es la que será mostrada por la interfaz web de forma dinámica. 
-	 * Si la incidencia está marcada como peligrosa, se deberá dar un aviso por la interfaz para que los operarios se percaten de ello.
-	 *  
-	 */
+
 	@Override
 	public void receiveFilteredIncidence(Incidence incidence) {
 		listaincidencias.add(incidence);
