590ade89a2b809d9103a6d4731cd2c59e8d92d0e
Manuel <35900893+UO2999@users.noreply.github.com>
2018-04-30 21:30:24 +0200

    Ampliamos el tiempo entre incidencias programadas
    Se amplia el tiempo entre incidencias para dar tiempo a cargar la página que las muestra.

--- InciDashboard_e5b/src/main/java/uo/asw/websocket/services/ScheduleTask.java
+++ InciDashboard_e5b/src/main/java/uo/asw/websocket/services/ScheduleTask.java
@@ -14,7 +14,7 @@ public class ScheduleTask {
     private SimpMessagingTemplate template;
 
     // this will send a message to an endpoint on which a client can subscribe
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 30000)
     public void trigger() {
     	//Creao que aqui se envia la peticion correspondiente junto con la ahora a la 
     	//que se realizó dicha petición. ¿Esto puede ser asi?
